f8947208cfcee1b71b4453b299a23a1b3c8e7581
christophe <christophegueroult@gmail.com>
2021-03-23 10:56:28 +0100

    install and use fontAwesome

--- src/app/clients/pages/page-add-client/page-add-client.component.ts
+++ src/app/clients/pages/page-add-client/page-add-client.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-page-add-client',
   templateUrl: './page-add-client.component.html',
-  styleUrls: ['./page-add-client.component.scss']
+  styleUrls: ['./page-add-client.component.scss'],
 })
 export class PageAddClientComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
