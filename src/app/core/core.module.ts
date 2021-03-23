a49232243ebd076c2b7cfad0295d34ee1c788a50
christophe <christophegueroult@gmail.com>
2021-03-23 11:27:43 +0100

    header, nav and footer with ng-content simple

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 import { IconsModule } from '../icons/icons.module';
 import { LoginModule } from '../login/login.module';
 import { UiModule } from '../ui/ui.module';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent, NavComponent, FooterComponent],
   imports: [CommonModule],
-  exports: [UiModule, LoginModule, IconsModule],
+  exports: [UiModule, LoginModule, IconsModule, HeaderComponent, NavComponent, FooterComponent],
 })
 export class CoreModule {}
