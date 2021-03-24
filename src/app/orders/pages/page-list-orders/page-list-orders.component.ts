08320af01b801d9943b71975f6b78ac0a2f6c97d
christophe <christophegueroult@gmail.com>
2021-03-24 10:18:21 +0100

    http client and first http.get

--- src/app/orders/pages/page-list-orders/page-list-orders.component.ts
+++ src/app/orders/pages/page-list-orders/page-list-orders.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from 'src/app/core/models/order';
+import { OrdersService } from 'src/app/core/services/orders.service';
 
 @Component({
   selector: 'app-page-list-orders',
   templateUrl: './page-list-orders.component.html',
-  styleUrls: ['./page-list-orders.component.scss']
+  styleUrls: ['./page-list-orders.component.scss'],
 })
 export class PageListOrdersComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public collection!: Order[];
+  constructor(private ordersService: OrdersService) {
+    this.ordersService.collection.subscribe((flux) => {
+      console.log(flux);
+    });
   }
 
+  ngOnInit(): void {}
 }
